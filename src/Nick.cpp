cb2692540d3e7fc5d0d15bc165723d861f141ea2
Alice Li <alli@c1r5p8.hive.fi>
2025-01-13 16:05:58 +0200

    working with 2 same nickname

--- src/Nick.cpp
+++ src/Nick.cpp
@@ -6,7 +6,7 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 13:17:19 by alli              #+#    #+#             */
-/*   Updated: 2025/01/13 12:44:36 by alli             ###   ########.fr       */
+/*   Updated: 2025/01/13 16:01:54 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,71 @@
 
 bool Serv::addNickname(int fd, std::vector<std::string> tokens)
 {
-	if (tokens.size() > 1)
-	{
-		std::string error_nick = std::string("please only input 1 nickname ") + "\r\n";
-		send(fd, error_nick.c_str(), error_nick.size(), 0);
-	}
-	// for (unsigned long i = 0; i < clients.size(); i++)
-	if (clients[fd].getFd() == fd)
-	{
-			std::string nickname = tokens[0];
-			// std::cout << "fd in addnickname" << fd << std::endl;
-			if (clients[fd].getNickname().empty()) // new nickname if there's no nickname
-			{
-				if (uniqueNickname(nickname) == true)
-				{
-					clients[fd].setNickname(nickname);
-					std::string nick = " :ircserver 001 " + clients[fd].getNickname() + " added to network " + "\r\n";
-					if (send(fd, nick.c_str(), nick.size(), 0) == -1)
-						return false;
-					return true;
-				}
-				else
-					return false;
-			}
-			if (uniqueNickname(nickname) == true) //replacing nickname
-			{
-				std::cout << "unique nickname here" << std::endl;
-				std::string oldName = clients[fd].getNickname();
-				std::cout << oldName << std::endl;
-				clients[fd].setNickname(nickname);
-				std::string nick = ":" + oldName + " NICK " + clients[fd].getNickname() +  "\r\n";
-				if (send(fd, nick.c_str(), nick.size(), 0) == -1)
-						return false;
-				return true;
-			}
-			else
-			{
+    if (tokens.size() > 1)
+    {
+        std::string error_nick = std::string("please only input 1 nickname ") + "\r\n";
+        send(fd, error_nick.c_str(), error_nick.size(), 0);
+    }
+    // for (unsigned long i = 0; i < clients.size(); i++)
+    if (clients[fd].getFd() == fd)
+    {
+            std::string nickname = tokens[0];
+            // std::cout << "fd in addnickname" << fd << std::endl;
+            if (clients[fd].getNickname().empty()) // new nickname if there's no nickname
+            {
+				std::cout << "entered empty nickname" << std::endl;
+                if (uniqueNickname(nickname) == true)
+                {
+                    clients[fd].setNickname(nickname);
+                    std::string nick = " :ircserver 001 " + clients[fd].getNickname() + " added to network " + "\r\n";
+                    if (send(fd, nick.c_str(), nick.size(), 0) == -1)
+                        return false;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else if (uniqueNickname(nickname) == true || clients.size() == 1) //replacing nickname
+            {
+                std::cout << "unique nickname here" << std::endl;
+                std::string oldName = clients[fd].getNickname();
+                std::cout << oldName << std::endl;
+                clients[fd].setNickname(nickname);
+                std::string nick = ":" + oldName + " NICK " + clients[fd].getNickname() +  "\r\n";
+                if (send(fd, nick.c_str(), nick.size(), 0) == -1) 
+                        return false;
+                return true;
+            }
+            else if (uniqueNickname(nickname) == false && clients[fd].welcomeSent == false)
+            {
+				std::cout << "entered add nickname, false and welcome false" << std::endl;
+                return true;
+            }
+			else 
 				return false;
-			}
-		}
-	return false;
+        }
+    return false;
 }
 
 bool Serv::uniqueNickname(std::string nickname)
 {
-	std::cout << "client size in unique nickname" << clients.size() << std::endl;
-	for(const auto& [fd, client] : clients)
-	{
-		if (client.getNickname() == nickname)
-		{
-			std::cerr << "nickname is taken, please choose another one" << std::endl;
-			return false;
-		}
-		else
-			continue;
-	}
-	return true;
+    // std::cout << "client size in unique nickname" << clients.size() << std::endl;
+    for(auto& [fd, client] : clients)
+    {
+		std::cout << "in unique nickname: " << nickname << std::endl;
+		std::cout << "client.getnickname: " << client.getNickname() << std::endl;
+        if (client.getNickname() == nickname)
+        {
+           if(client.welcomeSent == false || clients.size() > 1)
+		   {
+				std::cout << "entered unique nickname welcome sent false" << std::endl;
+				client.setNickname(nickname + "_");
+				return true;
+		   }
+		   return false;
+        }
+        else
+            continue;
+    }
+    return true;
 }
--- src/WelcomeMsg.cpp
+++ src/WelcomeMsg.cpp
@@ -6,7 +6,7 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 12:16:25 by alli              #+#    #+#             */
-/*   Updated: 2025/01/10 12:25:57 by alli             ###   ########.fr       */
+/*   Updated: 2025/01/13 15:38:44 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,32 @@
 
 void Serv::sendWelcomeMsg(int fd)
 {
-	clients[fd].allSet = true;
-	if (!clients[fd].getUsername().empty() && !clients[fd].getNickname().empty())
-	{
-		std::cout << "777" << std::endl;
-		if(clients[fd].welcomeSent == false  )
+    // clients[fd].allSet = true;
+    //     std::cout << "777" << std::endl;
+    // std::cout << "FD"<<fd<< std::endl;
+    // std::cout << "GetUsername => "<<clients[fd].getUsername() << "<="<< std::endl;
+    // std::cout << "GetNicjname"<<clients[fd].getNickname()<< std::endl;
+    
+    if (!clients[fd].getUsername().empty() && !clients[fd].getNickname().empty())
+    {
+        if (uniqueNickname(clients[fd].getNickname()) == true)
 		{
-			std::cout << "8888" << std::endl;
-			std::string nick = " :ircserver 001 " + clients[fd].getNickname() + " :Welcome to network, " + clients[fd].getNickname() + "@localhost" + "\r\n";
-			send(fd, nick.c_str(), nick.size(), 0);
-			std::string nick1 = " :ircserver 002 " + clients[fd].getNickname() + " :Your host is localhost, running version 1.0\r\n ";
-			send(fd, nick1.c_str(), nick1.size(), 0);
-			std::string nick2 = " :ircserver 003 " + clients[fd].getNickname() + " :This server was created today\r\n ";
-			send(fd, nick2.c_str(), nick2.size(), 0);
-			std::string nick3 = " :ircserver 004 " + clients[fd].getNickname() + " localhost 1.0 iow\r\n " + clients[fd].getNickname() + "@localhost" + "\r\n";
-			send(fd, nick3.c_str(), nick3.size(), 0);
-				
-			//welcome message
-			clients[fd].welcomeSent = true;
+			if(clients[fd].welcomeSent == false)
+			{
+				std::cout << "8888" << std::endl;
+				std::string nick = ":ircserver 001 " + clients[fd].getNickname() + " :Welcome to network, " + clients[fd].getNickname() + "@localhost" + "\r\n";
+				send(fd, nick.c_str(), nick.size(), 0);
+				std::string nick1 = ":ircserver 002 " + clients[fd].getNickname() + " :Your host is localhost, running version 1.0\r\n ";
+				send(fd, nick1.c_str(), nick1.size(), 0);
+				std::string nick2 = ":ircserver 003 " + clients[fd].getNickname() + " :This server was created today\r\n ";
+				send(fd, nick2.c_str(), nick2.size(), 0);
+				std::string nick3 = ":ircserver 004 " + clients[fd].getNickname() + " localhost 1.0 iow\r\n " + clients[fd].getNickname() + "@localhost" + "\r\n";
+				send(fd, nick3.c_str(), nick3.size(), 0);
+					
+				//welcome message
+				clients[fd].welcomeSent = true;
 
+			}
 		}
-	}
+    }
 }
