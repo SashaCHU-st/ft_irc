22a2db4084e3aa709895f9fec9dd6f75c992920f
Alice Li <alli@c1r6p18.hive.fi>
2025-02-14 16:01:17 +0200

    working welcome message

--- src/WelcomeMsg.cpp
+++ src/WelcomeMsg.cpp
@@ -6,7 +6,7 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 12:16:25 by alli              #+#    #+#             */
-/*   Updated: 2025/02/14 12:53:23 by alli             ###   ########.fr       */
+/*   Updated: 2025/02/14 15:42:45 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,14 @@ void Serv::sendWelcomeMsg(int fd)
 			// send(fd, nick.c_str(), nick.size(), 0);
 			// std::string nick1 = ":" + clients[fd].getServerName() + " 002 " + clients[fd].getNickname() + " :Your host is " + clients[fd].getServerName( ) +  " running version 1.0 \r\n ";
 			// send(fd, nick1.c_str(), nick1.size(), 0);
-			std::string nick = " :" + clients[fd].getServerName() + " 001 " + clients[fd].getNickname() + " :Welcome to the network, " + clients[fd].getNickname() + "!" + clients[fd].getUsername() + "@" + clients[fd].getServerName() + "\r\n";
+			std::string nick = ":" + clients[fd].getServerName() + " 001 " + clients[fd].getNickname() + " :Welcome to the network, " + clients[fd].getNickname() + "!" + clients[fd].getUsername() + "@" + clients[fd].getServerName() + "\r\n";
 			std::cout << nick << std::endl;
 			send(fd, nick.c_str(), nick.size(), 0);
 
-			std::string nick1 = " :" + clients[fd].getServerName() + " 002 " + clients[fd].getNickname() + " :Your host is " + clients[fd].getServerName() + ", running version 1.0\r\n";  // 🚀 FIXED: Removed extra space before \r\n
+			std::string nick1 = ":" + clients[fd].getServerName() + " 002 " + clients[fd].getNickname() + " :Your host is " + clients[fd].getServerName() + ", running version 1.0\r\n";  // 🚀 FIXED: Removed extra space before \r\n
 			send(fd, nick1.c_str(), nick1.size(), 0);
 
-			std::string nick2 = " :" + clients[fd].getServerName() + " 003 " + clients[fd].getNickname() + " :This server was created today\r\n ";
+			std::string nick2 = ":" + clients[fd].getServerName() + " 003 " + clients[fd].getNickname() + " :This server was created today\r\n ";
 			send(fd, nick2.c_str(), nick2.size(), 0);
 			std::string nick3 = ":" + clients[fd].getServerName() + " 004 " + clients[fd].getNickname() + " :localhost 1.0 iow\r\n " + clients[fd].getNickname() + "@localhost" + "\r\n";
 			send(fd, nick3.c_str(), nick3.size(), 0);
