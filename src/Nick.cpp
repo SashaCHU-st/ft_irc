b626c50b94305126f7f616660991dfc75bdf2260
Alice Li <aliceli.bls@gmail.com>
2025-02-25 16:22:49 +0200

    changed the at sign for nickname

--- src/Nick.cpp
+++ src/Nick.cpp
@@ -6,7 +6,7 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 13:17:19 by alli              #+#    #+#             */
-/*   Updated: 2025/02/25 09:01:12 by alli             ###   ########.fr       */
+/*   Updated: 2025/02/25 16:21:18 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ bool Serv::addNickname(int fd, std::string name)
 		{
 			if (uniqueNickname(name) == true)
 			{
-				clients[fd].setNickname(name);
-				return true;
+				if (checkSigns(name) == true)
+				{
+					clients[fd].setNickname(name);
+					return true;
+				}
+				return false;
 			}
 			else if (!uniqueNickname(name))
 			{
@@ -50,11 +54,17 @@ bool Serv::addNickname(int fd, std::string name)
 		else if (uniqueNickname(name))
 		{
 			std::string oldName = clients[fd].getNickname();
-			clients[fd].setNickname(name);
-			std::string nick = ":" + oldName + " NICK " + clients[fd].getNickname() +  "\r\n";
-			if (send(fd, nick.c_str(), nick.size(), 0) == -1) 
+			if (checkSigns(name) == true)
+			{
+				clients[fd].setNickname(name);
+				std::string nick = ":" + oldName + " NICK " + clients[fd].getNickname() +  "\r\n";
+				if (send(fd, nick.c_str(), nick.size(), 0) == -1) 
 					return false;
-			return true;
+				return true;
+			}
+			return false;
+			// clients[fd].setNickname(name);
+			// return true;
 		}
 		else if (!uniqueNickname(name))
 		{
@@ -104,5 +114,25 @@ int	Serv::findLatestMatch(int client_fd, std::string nickname)
 			return client->first;
 	}
 	return -1;
+}
+bool Serv::checkSigns(std::string nickname)
+{
+	for (size_t i = 0; i < nickname.length(); i++)
+	{
+		int a = 0;
+		if (i == 0)
+		{
+			std::cout << "nickname[i]: " << nickname[i] << std::endl;
+			while (nickname[i] == '@')
+			{
+				a++;
+				i++;
+			}
+			std::cout <<"a: " << a << std::endl;
+			if (a >= 1)
+				return false;
+		}
+	}
+	return true;
 }
 	
